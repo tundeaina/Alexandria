1719893dce94a2d3ebeed0f853a68fa0e7d471c4
Tunde Aina <tundeaina@gmail.com>
2015-10-10 22:37:34 -0400

    For Submission

---
+++ app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -0,0 +1,252 @@
+package it.jaschke.alexandria;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import it.jaschke.alexandria.data.AlexandriaContract;
+import it.jaschke.alexandria.services.BookService;
+import it.jaschke.alexandria.services.DownloadImage;
+
+public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String TAG = "INTENT_TO_SCAN_ACTIVITY";
+    private EditText ean;
+    private TextView userFeedbackView;
+    private final int LOADER_ID = 1;
+    private View rootView;
+    private final String EAN_CONTENT="eanContent";
+    private final int SCAN_ID = 1;
+    public static final String MESSAGE_EVENT = "MESSAGE_EVENT";
+    public static final String MESSAGE_KEY = "MESSAGE_EXTRA";
+
+    public AddBook(){
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(ean!=null) {
+            outState.putString(EAN_CONTENT, ean.getText().toString());
+        }
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_add_book, container, false);
+        ean = (EditText) rootView.findViewById(R.id.ean);
+
+        userFeedbackView = (TextView) rootView.findViewById(R.id.userFeedback);
+
+        ean.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //no need
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //catch isbn10 numbers by prepending 978
+                if(s.length()==10 && !s.toString().startsWith("978") ){
+                    ean.setText("978"+s.toString());
+                    ean.setSelection(ean.getText().length());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                //Remove user message view
+                userFeedbackView.setVisibility(View.GONE);
+
+                String ean = s.toString();
+
+                if(ean.length()<4) {
+                    clearFields();
+                    return;
+                }
+
+                //Once we have an ISBN, then start a book intent
+                if (ean.length()==13) {
+
+                    //Check Network Connectivity,
+                    if(!isNetworkAvailable()){
+                        userFeedbackView.setText(getResources().getString(R.string.no_connectivity));
+                        userFeedbackView.setVisibility(View.VISIBLE);
+                        return;
+                    }
+
+                    InputMethodManager inputManager = (InputMethodManager)
+                            getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                    inputManager.hideSoftInputFromWindow(
+                            (rootView.findViewById(R.id.ean)).getWindowToken(), 0);
+
+                    clearFields();
+                    Intent bookIntent = new Intent(getActivity(), BookService.class);
+                    bookIntent.putExtra(BookService.EAN, ean);
+                    bookIntent.setAction(BookService.FETCH_BOOK);
+                    getActivity().startService(bookIntent);
+                    AddBook.this.restartLoader();
+
+                }
+
+            }
+        });
+
+        rootView.findViewById(R.id.scan_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent barcodeIntent = new Intent(getActivity(),
+                        BarcodeTrackerActivity.class);
+
+                startActivityForResult(barcodeIntent, SCAN_ID);
+
+            }
+        });
+
+        rootView.findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearFields();
+                ean.setText("");
+            }
+        });
+
+        rootView.findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearFields();
+                Intent bookIntent = new Intent(getActivity(), BookService.class);
+                bookIntent.putExtra(BookService.EAN, ean.getText().toString());
+                bookIntent.setAction(BookService.DELETE_BOOK);
+                getActivity().startService(bookIntent);
+                ean.setText("");
+            }
+        });
+
+        if(savedInstanceState!=null){
+            ean.setText(savedInstanceState.getString(EAN_CONTENT));
+            ean.setHint("");
+        }
+
+        Bundle args = getArguments();
+
+        if (args != null) {
+            ean.setText(args.getString("EAN"));
+        }
+
+        if(ean.getText().length()==0){
+            clearFields();
+        }
+
+        return rootView;
+    }
+
+    private void restartLoader(){
+        getLoaderManager().restartLoader(LOADER_ID, null, this);
+    }
+
+    @Override
+    public android.support.v4.content.Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if(ean.getText().length()==0){
+            return null;
+        }
+        String eanStr= ean.getText().toString();
+        if(eanStr.length()==10 && !eanStr.startsWith("978")){
+            eanStr="978"+eanStr;
+        }
+        return new CursorLoader(
+                getActivity(),
+                AlexandriaContract.BookEntry.buildFullBookUri(Long.parseLong(eanStr)),
+                null,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(android.support.v4.content.Loader<Cursor> loader, Cursor data) {
+        if (!data.moveToFirst()) {
+            return;
+        }
+
+        String bookTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
+        ((TextView) rootView.findViewById(R.id.bookTitle)).setText(bookTitle);
+
+        String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
+        ((TextView) rootView.findViewById(R.id.bookSubTitle)).setText(bookSubTitle);
+
+        String authors = data.getString(data.getColumnIndex(AlexandriaContract.AuthorEntry.AUTHOR));
+
+        if(authors != null) {
+            String[] authorsArr = authors.split(",");
+            ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
+            ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",", "\n"));
+        }
+        else{
+            ((TextView) rootView.findViewById(R.id.authors)).setText("");
+        }
+
+        String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
+        if(Patterns.WEB_URL.matcher(imgUrl).matches()){
+            new DownloadImage((ImageView) rootView.findViewById(R.id.bookCover)).execute(imgUrl);
+            rootView.findViewById(R.id.bookCover).setVisibility(View.VISIBLE);
+        }
+
+        String categories = data.getString(data.getColumnIndex(AlexandriaContract.CategoryEntry.CATEGORY));
+        ((TextView) rootView.findViewById(R.id.categories)).setText(categories);
+
+        rootView.findViewById(R.id.save_button).setVisibility(View.VISIBLE);
+        rootView.findViewById(R.id.delete_button).setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {
+
+    }
+
+    private void clearFields(){
+        ((TextView) rootView.findViewById(R.id.bookTitle)).setText("");
+        ((TextView) rootView.findViewById(R.id.bookSubTitle)).setText("");
+        ((TextView) rootView.findViewById(R.id.authors)).setText("");
+        ((TextView) rootView.findViewById(R.id.categories)).setText("");
+        rootView.findViewById(R.id.bookCover).setVisibility(View.INVISIBLE);
+        rootView.findViewById(R.id.save_button).setVisibility(View.INVISIBLE);
+        rootView.findViewById(R.id.delete_button).setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        activity.setTitle(R.string.scan);
+    }
+
+    //Based on a stackoverflow snippet
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+}
---
+++ app/src/main/java/it/jaschke/alexandria/BarcodeTrackerActivity.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.jaschke.alexandria;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import it.jaschke.alexandria.CameraPreview.CameraSourcePreview;
+import it.jaschke.alexandria.CameraPreview.GraphicOverlay;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+/**
+ * Activity for the barcode-tracker app.  This app barcodes with the rear facing
+ * camera, and draws overlay graphics to indicate the position, size, and ID of each face and
+ * barcode.
+ */
+public final class BarcodeTrackerActivity extends Activity {
+    private static final String TAG = "BarcodeTracker";
+
+    private CameraSource mCameraSource;
+    private CameraSourcePreview mPreview;
+    private GraphicOverlay mGraphicOverlay;
+
+    /**
+     * Initializes the UI and creates the detector pipeline.
+     */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+
+        mPreview = (CameraSourcePreview) findViewById(R.id.preview);
+        mGraphicOverlay = (GraphicOverlay) findViewById(R.id.faceOverlay);
+        Context context = getApplicationContext();
+
+        // A barcode detector is created to track barcodes.  An associated multi-processor instance
+        // is set to receive the barcode detection results, track the barcodes, and maintain
+        // graphics for each barcode on screen.  The factory is used by the multi-processor to
+        // create a separate tracker instance for each barcode.
+        BarcodeDetector barcodeDetector = new BarcodeDetector
+                .Builder(context)
+                .setBarcodeFormats(Barcode.EAN_13 | Barcode.EAN_8 | Barcode.UPC_A).build();
+
+        BarcodeTrackerFactory barcodeFactory = new BarcodeTrackerFactory(mGraphicOverlay,context);
+
+
+        barcodeDetector.setProcessor(
+                new MultiProcessor.Builder<>(barcodeFactory).build());
+
+        if (!barcodeDetector.isOperational()) {
+            // Note: The first time that an app using the barcode or face API is installed on a
+            // device, GMS will download a native libraries to the device in order to do detection.
+            // Usually this completes before the app is run for the first time.  But if that
+            // download has not yet completed, then the above call will not detect any barcodes
+            // and/or faces.
+            //
+            // isOperational() can be used to check if the required native libraries are currently
+            // available.  The detectors will automatically become operational once the library
+            // downloads complete on device.
+            Log.w(TAG, "Detector dependencies are not yet available.");
+        }
+
+
+        // Creates and starts the camera.  Note that this uses a higher resolution in comparison
+        // to other detection examples to enable the barcode detector to detect small barcodes
+        // at long distances.
+        mCameraSource = new CameraSource.Builder(context, barcodeDetector)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedPreviewSize(1600, 1024)
+                .setRequestedFps(15.0f)
+                .build();
+    }
+
+    /**
+     * Restarts the camera.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Register mMessageReceiver to receive messages.
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("BarcodeDetected"));
+
+        startCameraSource();
+    }
+
+    /**
+     * Stops the camera.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPreview.stop();
+        // Unregister since the activity is not visible
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+        super.onPause();
+    }
+
+    /**
+     * Releases the resources associated with the camera source, the associated detectors, and the
+     * rest of the processing pipeline.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCameraSource.release();
+    }
+
+    /**
+     * Starts or restarts the camera source, if it exists.  If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    private void startCameraSource() {
+        try {
+            mPreview.start(mCameraSource, mGraphicOverlay);
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to start camera source.", e);
+            mCameraSource.release();
+            mCameraSource = null;
+        }
+    }
+
+    // handler for received Intents for the "BarcodeDetected" event
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Extract data included in the Intent
+            String message = intent.getStringExtra("RawValue");
+            Log.d("receiver", "Got message: " + message);
+
+            Intent outIntent = new Intent(BarcodeTrackerActivity.this,
+                    MainActivity.class);
+
+            outIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+            startActivity(outIntent);
+
+        }
+    };
+
+}
+
+
+
+
+
+
+
+
+
